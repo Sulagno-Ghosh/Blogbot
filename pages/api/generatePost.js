3f4803e13eceff8010a9a20a8126dead10426308
Sulagno-Ghosh <sulagnoghosh889@gmail.com>
2023-09-26 19:13:19 -0230

    openai api working

--- pages/api/generatePost.js
+++ pages/api/generatePost.js
@@ -1,33 +1,34 @@
-import {Configuration, OpenAIApi} from 'openai';
+import {OpenAIApi, Configuration} from 'openai';
+
 
 
 
 export default async function handler(req, res) {
     const config = new Configuration({
-        apiKey: process.env.OPENAI_API_KEY
-    });
-
+      apiKey: process.env.OPENAI_API_KEY,
+    })
     const openai = new OpenAIApi(config);
 
-    const topic = "Top 10 gaming consoles";
-    const keywords = "Gaming, Playstation, Sony"
+    const topic = "Top 10 tips for dog owners";
+    const keywords = "first time dog owners, common dog health issues, best dog breeds";
 
     const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        temperature: 0.1,
-        max_tokens: 5000,
-        prompt: `Write a long and detailed SEO-friendly blog post about ${topic}, that targets the following comma-seperated keywords ${keywords}. 
-        The content should be formatted in SEO-friendly HTML.
-        The response must also include appropritate HTML title and meta description content.
-        The return format must be stringified JSON in the following format:
-        {
-            "postContent": post content here,
-            "title": title goes here,
-            "metaDescription": meta description goes here,
-        }`,
+      model: "text-davinci-003",
+      temperature: 0.1,
+      max_tokens: 3600,
+      prompt: `Write a long and detailed SEO-friendly blog post about ${topic}, that targets the following comma-seperated keywords ${keywords}. 
+      The content should be formatted in SEO-friendly HTML.
+      The response must also include appropritate HTML title and meta description content.
+      The return format must be stringified JSON in the following format:
+      {
+          "postContent": post content here,
+          "title": title goes here,
+          "metaDescription": meta description goes here,
+      }`
     });
-
-    console.log("response ", response)
-
-    res.status(200).json({post: response.data.choices[0]?.text})
-}
+    
+    console.log("response", response)
+    
+    res.status(200).json({ post: response.data.choices[0]?.text})
+  }
+  
--- pages/post/new.js
+++ pages/post/new.js
@@ -1,26 +1,26 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
-import { use, useState } from "react";
+
 import { AppLayout } from "../../components/AppLayout";
 
 
 export default function NewPost(props) {
    console.log('New post props: ', props)
-   const {postContent, setPostContent} = useState("")
+   
    const handleClick = async () =>{
-      const response = await fetch(`/api/generatePost`,{
-         method:'POST',
+
+      const response = await fetch('/api/generatePost',{
+         method: "POST",
       });
-      const json = await response.json()
-      console.log("Result : ",json.post.postContent);
-      setPostContent(json.post.postContent);
-   };
+      const json = await response.json();
+      console.log(json);
+   }
+   
     return (
        <div>
           <h1>this is the new post</h1>
           <button className="btn" onClick={handleClick}>
              Generate
           </button>
-          <div className = "max-w-screen-sm p-10" dangerouslySetInnerHTML={{__html: postContent}}/>
 
        </div>
     );
